6e29075baa1853745a91dc3afab5adc2867fc0a2
u <u>
2025-10-29 21:13:07 +0300

    update

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -1905,7 +1905,7 @@ void MainWindow::on_menu_copy_links_nkr_triggered() {
     show_log_impl(tr("Copied %1 item(s)").arg(links.length()));
 }
 
-std::shared_ptr<Configs::BuildConfigResult> MainWindow::BuildConfig(const std::shared_ptr<ProxyEntity> &ent, 
+std::shared_ptr<Configs::BuildConfigResult> MainWindow::BuildConfig(const std::shared_ptr<Configs::ProxyEntity> &ent, 
     bool forTest, bool forExport, int chainID = 0){
         return BuildConfig(ent, ruleSetMap, forTest, forExport, chainID);
 };
