2d315ef8ac51d4922faf08150e4341fe87964044
qr243vbi <qr243vbi@atomicmail.io>
2025-10-30 22:12:09 +0300

    update

--- cmake/nkr.cmake
+++ cmake/nkr.cmake
@@ -2,11 +2,11 @@ set(NKR_VERSION "DYNAMIC" CACHE STRING "A custom version string for application"
 set(NKR_DEFAULT_VERSION "1.0.0" CACHE STRING "A custom version string for application")
 
 if(NKR_VERSION STREQUAL "DYNAMIC")
-    add_compile_definitions(-DNKR_DYNAMIC_VERSION)
-    add_compile_definitions(-DNKR_DEFAULT_VERSION=\"${NKR_DEFAULT_VERSION}\")
+    add_compile_definitions(NKR_DYNAMIC_VERSION)
+    add_compile_definitions(NKR_DEFAULT_VERSION=\"${NKR_DEFAULT_VERSION}\")
     list(APPEND PLATFORM_SOURCES include/js/version.h src/js/version.cpp)
 else()
-    add_compile_definitions(-DNKR_VERSION=\"${NKR_VERSION}\")
+    add_compile_definitions(NKR_VERSION=\"${NKR_VERSION}\")
 endif()
 # Release
 
