8908be184117913fa8e78fe11d438c9c2b946ed0
qr243vbi <qr243vbi@atomicmail.io>
2025-10-30 11:07:25 +0300

    Add ./include/global/Configs.hpp

---
+++ include/global/Configs.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Const.hpp"
+#include "Utils.hpp"
+#include "ConfigItem.hpp"
+#include "DataStore.hpp"
+
+#include "keyvaluerange.h"
+
+// Switch core support
+
+namespace Configs {
+    QString FindCoreRealPath();
+
+    bool IsAdmin(bool forceRenew=false);
+
+    bool isSetuidSet(const std::string& path);
+
+    QString GetBasePath();
+} // namespace Configs
+
+#define ROUTES_PREFIX_NAME QString("route_profiles")
+#define ROUTES_PREFIX QString(ROUTES_PREFIX_NAME + "/")
