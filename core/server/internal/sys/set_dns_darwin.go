5664e32354292a386b75683a6bb0389fa7c1d939
qr243vbi <qr243vbi@atomicmail.io>
2025-10-30 11:07:24 +0300

    Add ./core/server/internal/sys/set_dns_darwin.go

---
+++ core/server/internal/sys/set_dns_darwin.go
@@ -0,0 +1,39 @@
+package sys
+
+import (
+	tun "github.com/sagernet/sing-tun"
+	E "github.com/sagernet/sing/common/exceptions"
+	"github.com/sagernet/sing/common/shell"
+	"strings"
+)
+
+func SetSystemDNS(addr string, interfaceMonitor tun.DefaultInterfaceMonitor) error {
+	interfaceName := interfaceMonitor.DefaultInterface().Name
+	interfaceDisplayName, err := getInterfaceDisplayName(interfaceName)
+	if err != nil {
+		return err
+	}
+
+	err = shell.Exec("/usr/sbin/networksetup", "-setdnsservers", interfaceDisplayName, addr).Attach().Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getInterfaceDisplayName(name string) (string, error) {
+	content, err := shell.Exec("/usr/sbin/networksetup", "-listallhardwareports").ReadOutput()
+	if err != nil {
+		return "", err
+	}
+	for _, deviceSpan := range strings.Split(string(content), "Ethernet Address") {
+		if strings.Contains(deviceSpan, "Device: "+name) {
+			substr := "Hardware Port: "
+			deviceSpan = deviceSpan[strings.Index(deviceSpan, substr)+len(substr):]
+			deviceSpan = deviceSpan[:strings.Index(deviceSpan, "\n")]
+			return deviceSpan, nil
+		}
+	}
+	return "", E.New(name, " not found in networksetup -listallhardwareports")
+}
