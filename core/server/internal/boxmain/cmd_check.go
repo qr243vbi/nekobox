396726242c0d4251168f84bcd029c2e11e246d67
qr243vbi <qr243vbi@atomicmail.io>
2025-10-30 11:07:24 +0300

    Add ./core/server/internal/boxmain/cmd_check.go

---
+++ core/server/internal/boxmain/cmd_check.go
@@ -0,0 +1,27 @@
+package boxmain
+
+import (
+	"context"
+	"github.com/sagernet/sing-box/include"
+
+	"Core/internal/boxbox"
+)
+
+func Check(content []byte) error {
+	ctx := context.Background()
+	ctx = boxbox.Context(ctx, include.InboundRegistry(), include.OutboundRegistry(), include.EndpointRegistry(), include.DNSTransportRegistry(), include.ServiceRegistry())
+	options, err := parseConfig(ctx, content)
+	if err != nil {
+		return err
+	}
+	ctx, cancel := context.WithCancel(ctx)
+	instance, err := boxbox.New(boxbox.Options{
+		Context: ctx,
+		Options: *options,
+	})
+	if err == nil {
+		instance.Close()
+	}
+	cancel()
+	return err
+}
