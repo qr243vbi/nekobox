21b78d640e826de3bfc998dddedf24065cafa830
qr243vbi <qr243vbi@atomicmail.io>
2025-10-30 11:04:16 +0300

    update

--- cmake/linux/linux.cmake
+++
@@ -1,2 +0,0 @@
-set(PLATFORM_SOURCES src/sys/linux/LinuxCap.cpp)
-set(PLATFORM_LIBRARIES dl)
--- core/server/internal/boxmain/debug_stub.go
+++
@@ -1,7 +0,0 @@
-//go:build debug && !linux
-
-package boxmain
-
-func rusageMaxRSS() float64 {
-	return -1
-}
