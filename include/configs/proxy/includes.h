f2b18b8802a836aab47ca657a3588cdda061cc6d
qr243vbi <qr243vbi@atomicmail.io>
2025-10-30 11:07:25 +0300

    Add ./include/configs/proxy/includes.h

---
+++ include/configs/proxy/includes.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "SocksHttpBean.hpp"
+#include "ShadowSocksBean.hpp"
+#include "ChainBean.hpp"
+#include "VMessBean.hpp"
+#include "TrojanVLESSBean.hpp"
+#include "QUICBean.hpp"
+#include "AnyTLSBean.hpp"
+#include "WireguardBean.h"
+#include "Tailscale.hpp"
+#include "SSHBean.h"
+#include "CustomBean.hpp"
+#include "ExtraCore.h"
