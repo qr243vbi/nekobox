34d4d5743909d92a4c62091d3a310a4a8bf07ab8
qr243vbi <qr243vbi@atomicmail.io>
2025-10-30 11:07:22 +0300

    Add ./cmake/nkr.cmake

---
+++ cmake/nkr.cmake
@@ -0,0 +1,20 @@
+set(NKR_VERSION "DYNAMIC" CACHE STRING "A custom version string for application")
+set(NKR_DEFAULT_VERSION "1.0.0" CACHE STRING "A custom version string for application")
+
+if(NKR_VERSION STREQUAL "DYNAMIC")
+    add_compile_definitions(-DNKR_DYNAMIC_VERSION)
+    add_compile_definitions(-DNKR_DEFAULT_VERSION=\"${NKR_DEFAULT_VERSION}\")
+    list(APPEND PLATFORM_SOURCES include/js/version.h src/js/version.cpp)
+else()
+    add_compile_definitions(-DNKR_VERSION=\"${NKR_VERSION}\")
+endif()
+# Release
+
+# Debug
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -DNKR_CPP_DEBUG")
+
+# Func
+function(nkr_add_compile_definitions arg)
+    message("[add_compile_definitions] ${ARGV}")
+    add_compile_definitions(${ARGV})
+endfunction()
