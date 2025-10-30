e879c92293201e4f5d3ea8525bf9db26db4df6f9
qr243vbi <qr243vbi@atomicmail.io>
2025-10-30 11:04:16 +0300

    update

--- cmake/myproto.cmake
+++
@@ -1,9 +0,0 @@
-add_subdirectory(3rdparty/simple-protobuf)
-
-spb_protobuf_generate(PROTO_SRCS PROTO_HDRS core/server/gen/libcore.proto)
-
-add_library(myproto STATIC ${PROTO_SRCS} ${PROTO_HDRS})
-target_link_libraries(myproto
-        PUBLIC
-        spb-proto
-        )
--- include/sys/linux/LinuxCap.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <QString>
-
-bool Linux_HavePkexec();
-
-QString Linux_FindCapProgsExec(const QString &name);
-
-int Linux_Run_Command(const QString &commandName, const QString &args);
