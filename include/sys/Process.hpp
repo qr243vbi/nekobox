6378321efcff012d96887ef99a7f088e0d09f707
qr243vbi <qr243vbi@atomicmail.io>
2025-10-30 11:07:26 +0300

    Add ./include/sys/Process.hpp

---
+++ include/sys/Process.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <memory>
+#include <QElapsedTimer>
+#include <QProcess>
+
+#undef ELEVATE_METHOD
+#ifdef Q_OS_LINUX
+#define ELEVATE_METHOD
+#endif
+#ifdef Q_OS_WIN
+#define ELEVATE_METHOD
+#endif
+
+namespace Configs_sys {
+    class CoreProcess : public QProcess
+    {
+    public:
+        QString tag;
+        QString program;
+        QStringList arguments;
+
+        ~CoreProcess();
+
+        // start & kill is one time
+
+        void Start();
+
+        void Kill();
+
+        void Restart();
+
+#ifdef ELEVATE_METHOD
+        void elevateCoreProcessProgram();
+#endif
+
+        CoreProcess(const QString &core_path, const QStringList &args);
+
+        int start_profile_when_core_is_up = -1;
+
+    private:
+        bool show_stderr = false;
+        bool failed_to_start = false;
+        bool restarting = false;
+
+        QElapsedTimer coreRestartTimer;
+
+    protected:
+        bool started = false;
+        bool crashed = false;
+
+#ifdef ELEVATE_METHOD
+        bool coreProcessProgramElevated = false;
+#endif
+    };
+
+    inline QAtomicInt logCounter;
+} // namespace Configs_sys
