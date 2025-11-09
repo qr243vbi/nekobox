65920e8183acedb37edc37b73a63c600db6f5745
qr243vbi <qr243vbi@atomicmail.io>
2025-11-10 01:22:41 +0300

    update

--- script/deploy_linux64.sh
+++ script/deploy_linux64.sh
@@ -14,10 +14,10 @@ fi
 
 source script/env_deploy.sh
 DEST=$DEPLOYMENT/linux-$ARCH
-rm -rf $DEST
-mkdir -p $DEST
+mkdir -p $DEST ||:
 
 #### copy srslist ####
+wget -c https://github.com/qr243vbi/ruleset/raw/refs/heads/rule-set/srslist.json
 cp srslist.json $DEST/srslist.json
 
 #### copy binary ####
@@ -26,44 +26,50 @@ cp $BUILD/nekobox $DEST
 #### copy nekobox.png ####
 cp ./res/nekobox.ico $DEST/nekobox.ico
 
+(
 cd download-artifact
 cd *linux-$ARCH
 tar xvzf artifacts.tgz -C ../../
-cd ../..
+) ||:
 
-wget https://github.com/linuxdeploy/linuxdeploy/releases/download/1-alpha-20250213-2/linuxdeploy-$ARCH1.AppImage
-wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/1-alpha-20250213-1/linuxdeploy-plugin-qt-$ARCH1.AppImage
-chmod +x linuxdeploy-$ARCH1.AppImage linuxdeploy-plugin-qt-$ARCH1.AppImage
+[[ -x linuxdeploy-$ARCH1.AppImage ]] || wget -c https://github.com/linuxdeploy/linuxdeploy/releases/download/1-alpha-20250213-2/linuxdeploy-$ARCH1.AppImage
+[[ -x linuxdeploy-plugin-qt-$ARCH1.AppImage ]] || wget -c https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/1-alpha-20250213-1/linuxdeploy-plugin-qt-$ARCH1.AppImage
+[[ -f runtime-${ARCH1} ]] || wget -c https://github.com/AppImage/type2-runtime/releases/download/continuous/runtime-${ARCH1}
+[[ -x appimagetool-${ARCH1}.AppImage ]] || wget -c https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-${ARCH1}.AppImage
+chmod +x *.AppImage
 
 export EXTRA_QT_PLUGINS="iconengines;wayland-shell-integration;wayland-decoration-client;"
 export EXTRA_PLATFORM_PLUGINS="libqwayland.so;"
 ./linuxdeploy-$ARCH1.AppImage --appdir $DEST --executable $DEST/nekobox --plugin qt
-rm linuxdeploy-$ARCH1.AppImage linuxdeploy-plugin-qt-$ARCH1.AppImage
+
 cd $DEST
-rm -r ./usr/translations ./usr/bin ./usr/share ./apprun-hooks
+rm -r ./usr/translations ||:
+rm -r ./usr/bin ||:
+rm -r ./usr/share ||:
+rm -r ./apprun-hooks ||:
 
 # fix plugins rpath
-rm -r ./usr/plugins
+rm -r ./usr/plugins ||:
 mkdir ./usr/plugins
 mkdir ./usr/plugins/platforms
-cp $QT_PLUGIN_PATH/platforms/libqxcb.so ./usr/plugins/platforms
-cp $QT_PLUGIN_PATH/platforms/libqwayland.so ./usr/plugins/platforms
-cp -r $QT_PLUGIN_PATH/platformthemes ./usr/plugins
-cp -r $QT_PLUGIN_PATH/imageformats ./usr/plugins
-cp -r $QT_PLUGIN_PATH/iconengines ./usr/plugins
-cp -r $QT_PLUGIN_PATH/wayland-shell-integration ./usr/plugins
-cp -r $QT_PLUGIN_PATH/wayland-decoration-client ./usr/plugins
-cp -r $QT_PLUGIN_PATH/tls ./usr/plugins
-patchelf --set-rpath '$ORIGIN/../../lib' ./usr/plugins/platforms/libqxcb.so
-patchelf --set-rpath '$ORIGIN/../../lib' ./usr/plugins/platforms/libqwayland.so
-patchelf --set-rpath '$ORIGIN/../../lib' ./usr/plugins/platformthemes/libqgtk3.so
-patchelf --set-rpath '$ORIGIN/../../lib' ./usr/plugins/platformthemes/libqxdgdesktopportal.so
+cp $QT_PLUGIN_PATH/platforms/libqxcb.so ./usr/plugins/platforms ||:
+cp $QT_PLUGIN_PATH/platforms/libqwayland.so ./usr/plugins/platforms ||:
+cp -r $QT_PLUGIN_PATH/platformthemes ./usr/plugins ||:
+cp -r $QT_PLUGIN_PATH/imageformats ./usr/plugins ||:
+cp -r $QT_PLUGIN_PATH/iconengines ./usr/plugins ||:
+cp -r $QT_PLUGIN_PATH/wayland-shell-integration ./usr/plugins ||:
+cp -r $QT_PLUGIN_PATH/wayland-decoration-client ./usr/plugins ||:
+cp -r $QT_PLUGIN_PATH/tls ./usr/plugins ||:
+patchelf --set-rpath '$ORIGIN/../../lib' ./usr/plugins/platforms/libqxcb.so ||:
+patchelf --set-rpath '$ORIGIN/../../lib' ./usr/plugins/platforms/libqwayland.so ||:
+patchelf --set-rpath '$ORIGIN/../../lib' ./usr/plugins/platformthemes/libqgtk3.so ||:
+patchelf --set-rpath '$ORIGIN/../../lib' ./usr/plugins/platformthemes/libqxdgdesktopportal.so ||:
 
 # fix extra libs...
 mkdir ./usr/lib2
 ls ./usr/lib/
-cp ./usr/lib/libQt* ./usr/lib/libxcb-cursor* ./usr/lib/libxcb-util* ./usr/lib/libicuuc* ./usr/lib/libicui18n* ./usr/lib/libicudata* ./usr/lib2
-rm -r ./usr/lib
+cp ./usr/lib/libQt* ./usr/lib/libxcb-cursor* ./usr/lib/libxcb-util* ./usr/lib/libicuuc* ./usr/lib/libicui18n* ./usr/lib/libicudata* ./usr/lib2 ||:
+rm -r ./usr/lib ||:
 mv ./usr/lib2 ./usr/lib
 
 # fix lib rpath
@@ -78,10 +84,10 @@ shopt -s extglob
 
 mkdir -p appimage/AppDir
 cd appimage
-[[ -f "runtime-${ARCH1}" ]] || wget "https://github.com/AppImage/type2-runtime/releases/download/continuous/runtime-${ARCH1}"
-[[ -f "appimagetool-${ARCH1}.AppImage" ]] || wget "https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-${ARCH1}.AppImage"
+
+
 chmod 755 *
-cp -Rfv ../!(updater|appimage) ./AppDir
+cp -Rfv ../!(updater|appimage|nekobox-${ARCH1}.AppImage) ./AppDir
 (
 cd AppDir
 mv nekobox_core .nekobox_core_binary_file
@@ -97,15 +103,25 @@ export NEKOBOX_APPIMAGE_CUSTOM_EXECUTABLE="${NEKOBOX_APPIMAGE_CUSTOM_EXECUTABLE:
 exec "$(dirname $0)"/"${NEKOBOX_APPIMAGE_CUSTOM_EXECUTABLE}" "${@}"
 EOF
 cat << 'EOF' > updater
-#!/bin/sh
-rm "${APPIMAGE}"
+#!/bin/sh -x
+APPIMAGE="${2}"
+B=1
+OLD=${APPIMAGE}.old.${OLD}.AppImage
+while [[ -e "${OLD}" ]];
+do
+  B=$((B + 1))
+  OLD=${APPIMAGE}.old.${OLD}.AppImage
+done
+mv "${APPIMAGE}" "${OLD}"
 cp "${1}" "${APPIMAGE}"
+chmod 755 "${APPIMAGE}"
 shift
 shift
 "${APPIMAGE}" "${@}"
 EOF
 chmod 755 nekobox_core AppRun updater
 )
+rm ./AppDir/Tun.png ||:
 ln -s public/Tun.png ./AppDir/Tun.png
 cat << EOF > ./AppDir/nekobox.desktop
 [Desktop Entry]
@@ -118,6 +134,8 @@ Keywords=Internet;VPN;Proxy;sing-box;
 Exec=nekobox
 Icon=Tun
 EOF
-./appimagetool-${ARCH1}.AppImage AppDir ../nekobox-${ARCH1}.AppImage --runtime-file runtime-${ARCH1}
+rm -rfv *.old.* ||:
+$CURDIR/appimagetool-${ARCH1}.AppImage AppDir ../nekobox-${ARCH1}.AppImage --runtime-file $CURDIR/runtime-${ARCH1}
+
 cd ../
 rm -rfv appimage
