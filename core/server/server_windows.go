3bcb64df1ea72c11a4d71bbf1080431df3c5e112
qr243vbi <qr243vbi@atomicmail.io>
2025-11-10 21:58:23 +0300

    update

--- core/server/server_windows.go
+++ core/server/server_windows.go
@@ -256,5 +256,13 @@ func (s *server) IsPrivileged(in *gen.EmptyReq, out *gen.IsPrivilegedResponse) e
 }
 
 func restartAsAdmin(){
+	elevated, _ := isElevated();
+	if (!elevated){
+		code, err := runAdmin(_port, _debug)
+		if (err != nil){
+			fmt.Fprintf(os.Stderr, "Failed to run as admin: %v\n", err)
+		}
+		os.Exit(code)
+	}
 }
 
