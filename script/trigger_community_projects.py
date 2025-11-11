0b60d822b06a76716e90e3570bfcf47406a1bb04
qr243vbi <qr243vbi@atomicmail.io>
2025-11-11 20:13:42 +0300

    update

---
+++ script/trigger_community_projects.py
@@ -0,0 +1,10 @@
+from copr import v3; 
+import os
+login=os.getenv("COPR_LOGIN")
+token=os.getenv("COPR_TOKEN")
+client = v3.Client({
+   'copr_url': 'https://copr.fedorainfracloud.org', 
+   'login': login, 
+   'token': token
+})
+ print(client.package_proxy.build('qr243vbi', 'NekoBox', 'NekoBox'))
