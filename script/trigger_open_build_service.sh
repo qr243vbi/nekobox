aff2d717c36325a587be73a6150a362edb166430
qr243vbi <qr243vbi@atomicmail.io>
2025-11-11 20:42:03 +0300

    update

--- script/trigger_open_build_service.sh
+++ script/trigger_open_build_service.sh
@@ -1,9 +1,9 @@
-#!/bin/bash -x
+#!/bin/bash
 echo '[general]' > oscrc
 echo 'apiurl=https://api.opensuse.org' >> oscrc
 echo '[https://api.opensuse.org]' >> oscrc
-echo 'user=${OBS_USER}' >> oscrc
-echo 'pass=${OBS_PASSWORD}' >> oscrc
+echo "user=${OBS_USER}" >> oscrc
+echo "pass=${OBS_PASSWORD}" >> oscrc
 echo 'credentials_mgr_class=osc.credentials.PlaintextConfigFileCredentialsManager' >> oscrc
 
 install -Dm644 oscrc "$HOME"/.config/osc/oscrc
