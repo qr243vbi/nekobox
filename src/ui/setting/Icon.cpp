46cd295c20316f73da5dfd74368754394378ba93
qr243vbi <qr243vbi@atomicmail.io>
2025-11-18 21:16:55 +0300

    NekoRay Is Alive

--- src/ui/setting/Icon.cpp
+++ src/ui/setting/Icon.cpp
@@ -6,50 +6,42 @@
 #include <QDir>
 
 #ifndef SYSTRAY_ICON_DIR
-#ifdef Q_OS_MACOS
-#define SYSTRAY_ICON_DIR ":/systray/"
-#define SYSTRAY_ICON(X) QString(SYSTRAY_ICON_DIR) + X
-#else
 #include "include/sys/Settings.h"
 #define SYSTRAY_ICON(X) getResource(X)
-#endif
 #endif
 
 #define FORMAT nullptr
 
 QPixmap Icon::GetTrayIcon(TrayIconStatus status) {
     QPixmap pixmap;
+    auto pixmap_read = QPixmap(SYSTRAY_ICON("On.png"), FORMAT);
+    if (!pixmap_read.isNull()) pixmap = pixmap_read;
 
-    if (status == NONE)
-    {
-        auto pixmap_read = QPixmap(SYSTRAY_ICON("Off.png"), FORMAT);
-        if (!pixmap_read.isNull()) pixmap = pixmap_read;
-    } else if (status == RUNNING)
-    {
-        auto pixmap_read = QPixmap(SYSTRAY_ICON("On.png"), FORMAT);
-        if (!pixmap_read.isNull()) pixmap = pixmap_read;
-    } else if (status == SYSTEM_PROXY_DNS)
-    {
-        auto pixmap_read = QPixmap(SYSTRAY_ICON("Proxy-Dns.png"), FORMAT);
-        if (!pixmap_read.isNull()) pixmap = pixmap_read;
-    } else if (status == SYSTEM_PROXY)
-    {
-        auto pixmap_read = QPixmap(SYSTRAY_ICON("Proxy.png"), FORMAT);
-        if (!pixmap_read.isNull()) pixmap = pixmap_read;
-    } else if (status == DNS)
-    {
-        auto pixmap_read = QPixmap(SYSTRAY_ICON("Dns.png"), FORMAT);
-        if (!pixmap_read.isNull()) pixmap = pixmap_read;
-    } else if (status == VPN)
-    {
-        auto pixmap_read = QPixmap(SYSTRAY_ICON("Tun.png"), FORMAT);
-        if (!pixmap_read.isNull()) pixmap = pixmap_read;
-    } else
-    {
-        MW_show_log("Icon::GetTrayIcon: Unknown status");
-        auto pixmap_read = QPixmap(SYSTRAY_ICON("Off.png"), FORMAT);
-        if (!pixmap_read.isNull()) pixmap = pixmap_read;
+    if (status == TrayIconStatus::NONE) return pixmap;
+
+    auto p = QPainter(&pixmap);
+    auto side = pixmap.width();
+    auto radius = side * 0.4;
+    auto d = side * 0.4;
+    auto margin = side * 0.04;
+
+    if (status == TrayIconStatus::RUNNING) {
+        p.setBrush(QBrush(Qt::darkGreen));
+    } else if (status == TrayIconStatus::SYSTEM_PROXY) {
+        p.setBrush(QBrush(Qt::blue));
+    } else if (status == TrayIconStatus::VPN) {
+        p.setBrush(QBrush(Qt::red));
+    } else if (status == TrayIconStatus::DNS) {
+        p.setBrush(QBrush(Qt::darkMagenta));
     }
+    p.drawRoundedRect(
+        QRect(side - d - margin,
+              side - d - margin,
+              d,
+              d),
+              radius,
+              radius);
+    p.end();
 
     return pixmap;
 }
