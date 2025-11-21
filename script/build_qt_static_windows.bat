776d709ebb9976b16ff0f89fb07e961fc38d756d
qr243vbi <qr243vbi@atomicmail.io>
2025-11-21 15:19:09 +0300

    update

--- script/build_qt_static_windows.bat
+++ script/build_qt_static_windows.bat
@@ -1,5 +1,5 @@
 cd qt6
-CALL .\configure.bat -openssl-linked -release -static -prefix ../qt6_build -static-runtime -submodules qtdeclarative,qtbase,qtimageformats,qtsvg,qttranslations,qtgrpc -skip tests -skip examples -gui -widgets -- -D OPENSSL_ROOT_DIR="%VCPKG_ROOT%/installed/x64-mingw-static"
+CALL .\configure.bat -openssl-linked -release -static -prefix ../qt6_build -static-runtime -submodules qtdeclarative,qtbase,qtimageformats,qtsvg,qttranslations,qtgrpc -skip tests -skip examples -gui -widgets -- -D CMAKE_TOOLCHAIN_FILE=%CD%/vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=%1 -DVCPKG_MANIFEST_MODE=OFF
 
 echo config complete, building...
 cmake --build . --parallel
