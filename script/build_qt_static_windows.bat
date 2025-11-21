af0c7ddf55fb86c38d0cc1af5a3fad680b8f889d
qr243vbi <qr243vbi@atomicmail.io>
2025-11-21 15:30:20 +0300

    update

--- script/build_qt_static_windows.bat
+++ script/build_qt_static_windows.bat
@@ -1,5 +1,6 @@
+set VCPKG_TOOLCHAIN=%CD%/vcpkg/scripts/buildsystems/vcpkg.cmake
 cd qt6
-CALL .\configure.bat -openssl-linked -release -static -prefix ../qt6_build -static-runtime -submodules qtdeclarative,qtbase,qtimageformats,qtsvg,qttranslations,qtgrpc -skip tests -skip examples -gui -widgets -- -D CMAKE_TOOLCHAIN_FILE=%CD%/vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=%1 -DVCPKG_MANIFEST_MODE=OFF
+CALL .\configure.bat -openssl-linked -release -static -prefix ../qt6_build -static-runtime -submodules qtdeclarative,qtbase,qtimageformats,qtsvg,qttranslations,qtgrpc -skip tests -skip examples -gui -widgets -- -D CMAKE_TOOLCHAIN_FILE=%VCPKG_TOOLCHAIN% -D VCPKG_TARGET_TRIPLET=%1 -D VCPKG_MANIFEST_MODE=OFF
 
 echo config complete, building...
 cmake --build . --parallel
