6304812938123f19645a093a02206770710ea07e
qr243vbi <qr243vbi@atomicmail.io>
2025-11-21 11:14:12 +0300

    update

--- script/build_qt_static_windows.bat
+++ script/build_qt_static_windows.bat
@@ -1,5 +1,5 @@
 cd qt6
-CALL .\configure.bat -openssl-linked -release -static -prefix ../qt6_build -static-runtime -submodules qtbase,qtimageformats,qtsvg,qttranslations,qtgrpc -skip tests -skip examples -gui -widgets -- -D OPENSSL_ROOT_DIR="%VCPKG_ROOT%/installed/x64-mingw-static"
+CALL .\configure.bat -openssl-linked -release -static -prefix ../qt6_build -static-runtime -submodules qtdeclarative,qtbase,qtimageformats,qtsvg,qttranslations,qtgrpc -skip tests -skip examples -gui -widgets -- -D OPENSSL_ROOT_DIR="%VCPKG_ROOT%/installed/x64-mingw-static"
 
 echo on branch %1
 echo config complete, building...
