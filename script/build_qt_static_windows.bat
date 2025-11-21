b7dd1f47c4b27b9b24fffef993b36b44795d666c
qr243vbi <qr243vbi@atomicmail.io>
2025-11-21 13:31:52 +0300

    update

--- script/build_qt_static_windows.bat
+++ script/build_qt_static_windows.bat
@@ -1,9 +1,8 @@
 cd qt6
 CALL .\configure.bat -openssl-linked -release -static -prefix ../qt6_build -static-runtime -submodules qtdeclarative,qtbase,qtimageformats,qtsvg,qttranslations,qtgrpc -skip tests -skip examples -gui -widgets -- -D OPENSSL_ROOT_DIR="%VCPKG_ROOT%/installed/x64-mingw-static"
 
-echo on branch %1
 echo config complete, building...
 cmake --build . --parallel
 echo build one, installing...
 ninja install
-echo installed Qt %1 in static mode
+echo installed Qt in static mode
