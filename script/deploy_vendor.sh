31425777b6227bcc17a4eeea01ca8ace72a3e2a0
qr243vbi <qr243vbi@atomicmail.io>
2025-12-27 01:53:09 +0300

    update

--- script/deploy_vendor.sh
+++ script/deploy_vendor.sh
@@ -2,7 +2,7 @@ source script/env_deploy.sh
 
 pushd "$SRC_ROOT"/core/server
 pushd gen
-    ./update_libs.sh
+ ./update_libs.sh
 popd
 curl -fLso "$SRC_ROOT/srslist.json" "https://github.com/qr243vbi/ruleset/raw/refs/heads/rule-set/srslist.json"
 go mod tidy
@@ -12,9 +12,9 @@ popd
 
 if [[ ! -d "$SRC_ROOT"/.git ]]
 then
-git init
-git add --all
-git commit -am "initial commit"
+ git init
+ git add --all
+ git commit -am "initial commit"
 fi
 
 echo $INPUT_VERSION > version.txt
@@ -22,7 +22,10 @@ echo $INPUT_VERSION > version.txt
 git add -f srslist* version.txt core/server/gen/*.go core/server/gen/libcore_service-remote core/server/vendor SingBox.Version
 git commit -am "New Update"
 
-mkdir "$DEPLOYMENT"
+if [[ ! -e "$DEPLOYMENT" ]]
+then
+ mkdir "$DEPLOYMENT"
+fi
 git ls-files --recurse-submodules | tar --transform="s,^,nekobox-unified-source-$INPUT_VERSION/,S" -c --xz -f "$DEPLOYMENT/nekobox-unified-source-$INPUT_VERSION.tar.xz" -T-
 sha256sum "$DEPLOYMENT/nekobox-unified-source-$INPUT_VERSION.tar.xz" > "$DEPLOYMENT/nekobox-unified-source-$INPUT_VERSION.tar.xz.sha256sum"
 
