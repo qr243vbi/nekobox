df286c4adf0ce8cf68b33ba239278e697b92ee21
u <u>
2026-01-08 15:10:47 +0300

    update

--- script/build_docker.sh
+++ script/build_docker.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 cmake -S . -B build -GNinja
-cmake --build build --verbose
-export GOOS=linux
-UNAME="$(uname -m)"
-
-if [[ "${UNAME}" == 'aarch64' || "${UNAME}" == 'arm64' ]]; then
-  GOARCH="arm64"
-else
-  GOARCH="amd64"
-fi
-export GOARCH
-
+cmake --build build -v
 ./script/build_go.sh
 ./script/deploy_linux64.sh
