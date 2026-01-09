82df0d9a30b8b8cccf5d7b777f9ccb924031a4e1
a <my@email.org>
2026-01-09 15:30:58 +0300

    New Update

--- script/build_docker.sh
+++ script/build_docker.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+. script/env_deploy.sh
+if [[ -d download-artifact ]]
+then
+(
+cd download-artifact
+cd *linux-$ARCH
+tar xvzf artifacts.tgz -C ../../
+) ||:
+fi
+
 cmake -S . -B build -GNinja
 cmake --build build -v
-./script/build_go.sh
-./script/deploy_linux64.sh
+(
+. script/build_go.sh
+)
+(
+. script/deploy_linux64.sh
+)
