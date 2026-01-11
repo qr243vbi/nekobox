2e289c9ce54b553e1ed84c4df7f1124d4fb7b043
qr243vbi <qr243vbi@atomicmail.io>
2026-01-12 01:20:04 +0300

    update

---
+++ core/server/vendor/github.com/sagernet/sing-box/constant/v2ray.go
@@ -0,0 +1,11 @@
+package constant
+
+const (
+	V2RayTransportTypeHTTP        = "http"
+	V2RayTransportTypeWebsocket   = "ws"
+	V2RayTransportTypeQUIC        = "quic"
+	V2RayTransportTypeGRPC        = "grpc"
+	V2RayTransportTypeHTTPUpgrade = "httpupgrade"
+	V2RayTransportTypeXHTTP       = "xhttp"
+	V2RayTransportTypeKCP         = "kcp"
+)
