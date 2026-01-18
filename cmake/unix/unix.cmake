81c1bdebfde2587a196cb5116f342e4e64bdfc3f
qr243vbi <qr243vbi@atomicmail.io>
2026-01-19 00:18:20 +0300

    Add support for windows 7, 8, 8.1

--- cmake/unix/unix.cmake
+++ cmake/unix/unix.cmake
@@ -2,4 +2,5 @@ set(PLATFORM_SOURCES
     src/sys/linux/LinuxCap.cpp
     ${NEKOBOX_INCLUDE}/sys/linux/LinuxCap.h
 )
-set(PLATFORM_LIBRARIES dl stdc++ m)
+find_package(Qt6 REQUIRED COMPONENTS DBus)
+set(PLATFORM_LIBRARIES dl stdc++ m Qt6::DBus)
