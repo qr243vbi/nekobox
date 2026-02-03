ee2be33bb585e12aaa873d98137dd973813196e8
qr243vbi <qr243vbi@atomicmail.io>
2026-02-04 03:26:10 +0800

    Update trigger_winget_pkgs.sh

--- script/trigger_winget_pkgs.sh
+++ script/trigger_winget_pkgs.sh
@@ -53,9 +53,6 @@ InstallerSwitches:
 InstallationMetadata:
   DefaultInstallLocation: '"'"'%AppData%\NekoBox'"'"'
 Installers:
-- Architecture: x86
-  InstallerUrl: https://github.com/qr243vbi/nekobox/releases/download/OldVer/nekobox-OldVer-windows32-installer.exe
-  InstallerSha256: OldSha32
 - Architecture: x64
   InstallerUrl: https://github.com/qr243vbi/nekobox/releases/download/OldVer/nekobox-OldVer-windows64-installer.exe
   InstallerSha256: OldSha64
@@ -66,6 +63,11 @@ ManifestType: installer
 ManifestVersion: 1.10.0
 
 '
+
+
+#- Architecture: x86
+#  InstallerUrl: https://github.com/qr243vbi/nekobox/releases/download/OldVer/nekobox-OldVer-windows32-installer.exe
+#  InstallerSha256: OldSha32
 
 MANIFEST3='# yaml-language-server: $schema=https://aka.ms/winget-manifest.defaultLocale.1.10.0.schema.json
 PackageIdentifier: qr243vbi.NekoBox
