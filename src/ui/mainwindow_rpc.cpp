34bdb813d9f82bce771e1118431f4a45925e9dcf
qr243vbi <qr243vbi@atomicmail.io>
2026-03-04 02:01:13 +0800

    Modify res/translations/eu_ES.ts

--- src/ui/mainwindow_rpc.cpp
+++ src/ui/mainwindow_rpc.cpp
@@ -38,9 +38,11 @@ void MainWindow::setup_rpc() {
     int port; 
     QString domain;
     if (Configs::windowSettings->use_tcp){
-        domain = 
+        domain = QString("127.0.0.1");
+        port = Configs::dataStore->core_port;
     } else {
-
+        domain = Configs::dataStore->core_unix_domain_socket;
+        port = -1;
     }
     defaultClient = new Client(
         [=](const QString &errStr) {
