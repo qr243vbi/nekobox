c5a5f9ddaa606b6acf70d7c9e6e556953edbaa03
qr243vbi <qr243vbi@atomicmail.io>
2026-03-16 03:45:03 +0800

    Modify res/translations/ps_AF.ts

--- res/translations/ps_AF.ts
+++ res/translations/ps_AF.ts
@@ -3825,7 +3825,7 @@ Release note:
         <location filename="../../src/ui/mainwindow.cpp" line="312" />
         <location filename="../../src/ui/mainwindow.cpp" line="1258" />
         <source>Download Profiles</source>
-        <translation type="unfinished" />
+        <translation type="unfinished">پروفایل ډاونلوډ کړئ</translation>
     </message>
 </context>
 <context>
@@ -3833,7 +3833,7 @@ Release note:
     <message>
         <location filename="../../src/nekobox/ui/security/delete_or_edit_users.ui" line="14" />
         <source>Form</source>
-        <translation type="unfinished" />
+        <translation type="unfinished">فورمه</translation>
     </message>
 </context>
 </TS>
