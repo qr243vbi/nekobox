032b90428a6201713a06842aaa4a7c58049ebe2e
qr243vbi <qr243vbi@atomicmail.io>
2026-03-16 21:17:31 +0800

    update

--- script/build_go.sh
+++ script/build_go.sh
@@ -22,7 +22,7 @@ fi; fi; fi; fi; fi; fi; fi;
 fi
 
 echo "DESTINATION IS $DEST FOR MACHINE $GOARCH with platform $GOOS"
-TAGS="with_clash_api,with_gvisor,with_quic,with_wireguard,with_utls,with_dhcp,with_tailscale,with_shadowtls,with_grpc,with_naive_outbound,with_purego,with_acme"
+TAGS="with_clash_api,with_gvisor,with_quic,with_wireguard,with_utls,with_dhcp,with_tailscale,with_shadowtls,with_grpc,with_naive_outbound,with_acme"
 
 GOCMD="${GOCMD:-go}"
 
