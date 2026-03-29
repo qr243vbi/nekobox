3867a5b04288d0c6cde9e3819275460df7c6e24d
qr243vbi <qr243vbi@atomicmail.io>
2026-03-30 03:28:09 +0800

    update

--- script/deploy_windows.sh
+++ script/deploy_windows.sh
@@ -37,18 +37,18 @@ fi
 pushd "$SRC_ROOT"
 
 #### get the pdb ####
-if [[ "$COMPILER" == "MinGW" ]]
-then
-curl -fLJO https://github.com/rainers/cv2pdb/releases/download/v0.53/cv2pdb-0.53.zip
-7z x cv2pdb-0.53.zip -ocv2pdb
-./cv2pdb/cv2pdb64.exe ./build/nekobox.exe ./tmp.exe ./nekobox.pdb
-rm -rf cv2pdb-0.53.zip cv2pdb
-cd build
-strip -s nekobox.exe
-cd ..
-rm tmp.exe ||:
-mv nekobox.pdb $DEST
-fi
+#if [[ "$COMPILER" == "MinGW" ]]
+#then
+#curl -fLJO https://github.com/rainers/cv2pdb/releases/download/v0.53/cv2pdb-0.53.zip
+#7z x cv2pdb-0.53.zip -ocv2pdb
+#./cv2pdb/cv2pdb64.exe ./build/nekobox.exe ./tmp.exe ./nekobox.pdb
+#rm -rf cv2pdb-0.53.zip cv2pdb
+#cd build
+#strip -s nekobox.exe
+#cd ..
+#rm tmp.exe ||:
+#mv nekobox.pdb $DEST
+#fi
 
 #### copy srslist ####
 if [[ ! -f srslist.json ]]
