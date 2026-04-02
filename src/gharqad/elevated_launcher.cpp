de4189a5ee784389adf9ce0dbd3cd32387fde54c
qr243vbi <qr243vbi@atomicmail.io>
2026-04-02 21:04:02 +0800

    update

--- src/gharqad/elevated_launcher.cpp
+++ src/gharqad/elevated_launcher.cpp
@@ -3,24 +3,108 @@
 #include <QLocalSocket>
 #include <QDateTime>
 #include <QDebug>
+#include <QJsonObject>
+#include <QProcess>
+#include <QJsonArray>
+#include <qcontainerfwd.h>
+#include <QFile>
+#include <QSettings>
+#include <QDir>
+#include <qcoreapplication.h>
+#include <QRandomGenerator>
+#include <QProcessEnvironment>
 
-QString handleCommand(const QString& cmd) {
-    if (cmd == "time") {
-        return QDateTime::currentDateTime().toString();
-    } else if (cmd == "hello") {
-        return "Hello from Qt server!";
-    } else {
-        return "ERROR: Command not allowed";
+QString handleCommand(const QString& cmd, const QString &program) {
+    auto obj = QJsonValue::fromJson(cmd.toUtf8());
+    QProcess process;
+    QStringList args;
+    QString ret = "";
+
+    QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
+    QStringList envs = env.toStringList();
+    
+    auto val = obj["args"];
+    if (val.isArray()){
+        for (QVariant var : val.toArray().toVariantList()){
+            args << var.toString();
+        }
     }
+
+    val = obj["envs"];
+    if (val.isArray()){
+        for (QVariant var : val.toArray().toVariantList()){
+            envs << var.toString();
+        }
+    }
+
+    if (args.length() > 0) {
+        process.setArguments(args);
+    }
+    if (envs.length() > 0) {
+        process.setEnvironment(envs);
+    }
+    if (!program.isEmpty()){
+        if (QFile(program).exists()){
+            process.setProgram(program);
+            process.startDetached();
+            ret = "ok";
+        }
+    }
+
+    return ret;
+}
+
+void unregister_task(QString task){
+
+}
+
+void register_task(QString task){
+
+}
+
+QString generateRandomString(int length)
+{
+    const QString chars = "abcdefghijklmnopqrstuvwxyz";
+    QString result;
+
+    for (int i = 0; i < length; ++i) {
+        int index = QRandomGenerator::global()->bounded(chars.length());
+        result.append(chars.at(index));
+    }
+
+    return result;
 }
 
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
-
     QLocalServer server;
 
-    QString socketName = "MyQtSocket";
+    QSettings settings("qr243vbi", "iblis_elevated_launcher");
 
+    if (argc > 2){
+        QString command = argv[1];
+        if (command == "register"){ 
+            QString task = settings.value("task_name", "").toString();
+            if (!task.isEmpty()){
+                unregister_task(task);
+            }
+            register_task(task);
+            task = generateRandomString(15);
+            settings.setValue("socket", generateRandomString(28));
+            settings.setValue("program", QString(argv[2]));
+            settings.setValue("task_name", task);
+        }
+        
+        return 1;
+    }
+
+    QString socketName = settings.value("socket").toString();
+    QString program = settings.value("program").toString();
+
+    if (socketName.isEmpty()){
+        return 1;
+    }
+    
     // Remove existing socket (important on Linux/macOS)
     QLocalServer::removeServer(socketName);
 
@@ -31,16 +115,16 @@ int main(int argc, char *argv[]) {
 
     qDebug() << "Server listening on" << socketName;
 
-    QObject::connect(&server, &QLocalServer::newConnection, [&]() {
+    QObject::connect(&server, &QLocalServer::newConnection, [&, program]() {
         QLocalSocket *client = server.nextPendingConnection();
 
-        QObject::connect(client, &QLocalSocket::readyRead, [client]() {
+        QObject::connect(client, &QLocalSocket::readyRead, [client, program]() {
             QByteArray data = client->readAll();
             QString command = QString::fromUtf8(data).trimmed();
 
             qDebug() << "Received:" << command;
 
-            QString response = handleCommand(command);
+            QString response = handleCommand(command, program);
 
             client->write(response.toUtf8() + "\n");
             client->flush();
