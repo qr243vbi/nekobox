603851e0681be6aadcb937de8df1d958564d0ca7
qr243vbi <qr243vbi@atomicmail.io>
2026-05-01 02:24:10 +0800

    Update cmakelists.txt

--- cmake/nkr.cmake
+++ cmake/nkr.cmake
@@ -19,6 +19,26 @@ function(nkr_add_compile_definitions arg)
 endfunction()
 
 function(embeed_resources)
+	set(FILE_URL "https://github.com/qr243vbi/ruleset/raw/refs/heads/rule-set/srslist.json")
+	set(DEST_FILE "${CMAKE_SOURCE_DIR}/srslist.json")
+	if(NOT EXISTS "${DEST_FILE}")
+		message(STATUS "srslist.json not found in source dir, downloading: ${DEST_FILE}")
+		file(DOWNLOAD
+			"${FILE_URL}"
+			"${DEST_FILE}"
+			SHOW_PROGRESS
+			STATUS download_status
+			LOG download_log
+		)
+		list(GET download_status 0 status_code)
+		if(NOT status_code EQUAL 0)
+			message(STATUS
+				"Download failed: ${download_status}\n${download_log}"
+			)
+		endif()
+	else()
+		message(STATUS "File already exists: ${DEST_FILE}")
+	endif()
 	# Directory containing resources
 	set(STATIC_RESOURCE_DIR "${CMAKE_SOURCE_DIR}/res/public")
 	set(QRC_FILE "${CMAKE_BINARY_DIR}/nekoobox_static.qrc")
