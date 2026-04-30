8b3dabf75d981034e4b65ba8c914c84778152560
qr243vbi <qr243vbi@atomicmail.io>
2026-04-30 17:00:29 +0800

    update

--- cmake/windows/windows.cmake
+++ cmake/windows/windows.cmake
@@ -19,6 +19,30 @@ generate_product_version(
 add_definitions(-DUNICODE -D_UNICODE -DNOMINMAX)
 set(GUI_TYPE WIN32)
 if (MINGW)
+	# Directory containing resources
+	set(STATIC_RESOURCE_DIR "${CMAKE_SOURCE_DIR}/res/public")
+	set(QRC_FILE "${CMAKE_BINARY_DIR}/nekoobox_static.qrc")
+	# Get all files in the directory
+	file(GLOB_RECURSE STATIC_RESOURCE_FILES "${STATIC_RESOURCE_DIR}/*")
+	# Generate .qrc file
+	file(WRITE ${QRC_FILE} "<RCC>\n  <qresource prefix=\"/nekobox/\">\n")
+	foreach(FILE_PATH IN LISTS STATIC_RESOURCE_FILES)
+		# Make path relative to project root
+		file(RELATIVE_PATH REL_PATH "${CMAKE_SOURCE_DIR}/res" "${FILE_PATH}")
+		file(RELATIVE_PATH REL_FILE_PATH "${CMAKE_BINARY_DIR}" "${FILE_PATH}")
+		file(APPEND ${QRC_FILE} "    <file alias=\"${REL_PATH}\">${REL_FILE_PATH}</file>\n")
+	endforeach()
+	foreach(FILE_PATH IN LISTS QM_FILENAMES)
+		# Make path relative to project root
+		file(APPEND ${QRC_FILE} "    <file alias=\"public/${FILE_PATH}.qm\">${FILE_PATH}.qm</file>\n")
+	endforeach()
+	file(RELATIVE_PATH SRSLIST_PATH "${CMAKE_BINARY_DIR}" "${CMAKE_SOURCE_DIR}/srslist.json")
+	file(RELATIVE_PATH LANGUAGES_PATH "${CMAKE_BINARY_DIR}" "${CMAKE_SOURCE_DIR}/res/languages.txt")
+	file(APPEND ${QRC_FILE} "    <file alias=\"public/srslist.json\">${SRSLIST_PATH}</file>\n")
+	file(APPEND ${QRC_FILE} "    <file alias=\"public/languages.txt\">${LANGUAGES_PATH}</file>\n")
+	file(APPEND ${QRC_FILE} "  </qresource>\n</RCC>\n")
+	set(PLATFORM_SOURCES ${PLATFORM_SOURCES} "${QRC_FILE}")
+    add_compile_options("-static")
     if (NOT DEFINED MinGW_ROOT)
         set(MinGW_ROOT "C:/msys64/mingw64")
     endif ()
