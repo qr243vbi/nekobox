qr243vbi/nekobox

commit 60e1adccb46e17656a641e7426514d39293a3cf3

Fix group updater

src/gharqad/configs/sub/GroupUpdater.cpp
@@ -84,6 +84,17 @@ void RawUpdater::update(const QString &str3) {
   QList<QString> stack = {str3};
   QString str;
 
+  std::shared_ptr<Configs::ProxyEntity> ent;
+  bool needFix = true;
+
+  bool json_contains_outbounds;
+  bool json_contains_inbounds;
+  bool json_contains_endpoints;
+  QJsonParseError error{};
+  QJsonDocument jsonDocument;
+  bool json_ok;
+  QJsonObject json;
+
 ret_loop:
   if (stack.size() != 0) {
     str = stack.takeFirst();
@@ -103,30 +114,8 @@ ret_loop:
     }
   }
 
-  // Clash
-  if (str.contains("proxies:")) {
-    updateClash(str);
-    goto ret_loop;
-  }
-
-  // Wireguard Config
-  if (str.contains("[Interface]") && str.contains("[Peer]")) {
-    updateWireguardFileConfig(str);
-    goto ret_loop;
-  }
-
-  // is comment or too short
-  if (str.startsWith("//") || str.startsWith("#") || str.length() < 2) {
-    goto ret_loop;
-  }
-
-  bool json_contains_outbounds;
-  bool json_contains_inbounds;
-  bool json_contains_endpoints;
-  QJsonParseError error{};
-  QJsonDocument jsonDocument = QJsonDocument::fromJson(str.toUtf8(), &error);
-  bool json_ok = error.error == error.NoError;
-  QJsonObject json;
+  jsonDocument = QJsonDocument::fromJson(str.toUtf8(), &error);
+  json_ok = error.error == error.NoError;
   if (json_ok) {
     if (jsonDocument.isArray()) {
       for (auto i : jsonDocument.array()) {
@@ -164,6 +153,18 @@ ret_loop:
     goto parse_json;
   }
 
+  // Clash
+  if (updateClash(str)){
+    goto ret_loop;
+  }
+
+  // Wireguard Config
+  /*
+  if (updateWireguardFileConfig(str)){
+    goto ret_loop;
+  }
+  */
+
   // Multi line
   if (str.count("\n") > 0) {
     auto list = Disect(str);
@@ -175,8 +176,6 @@ ret_loop:
 
 parse_json:
 
-  std::shared_ptr<Configs::ProxyEntity> ent;
-  bool needFix = true;
 
   // Json
   if (json_ok) {
@@ -195,34 +194,29 @@ parse_json:
   }
 
   QString scheme;
-    bool quic_enabled = false;
+  bool quic_enabled = true;
 
-  int scheme_index = str.indexOf("://");
-#ifdef DEBUG_MODE
-  qDebug() << "scheme index is " << scheme_index;
-#endif
-
-  if (scheme_index > 0) {
-    scheme = str.sliced(0, scheme_index).toLower();
-#ifdef DEBUG_MODE
-    qDebug() << "Scheme Is : " << scheme;
-#endif
-  } else {
-    goto ret_loop;
+  QUrl scheme_link = QUrl(str.trimmed());
+  if (scheme_link.isValid()){
+    scheme = scheme_link.scheme();
+    quic_enabled = false;
   }
 
+  // skip if link is invalid
+  if (quic_enabled){
+
+  }
   // Nekoray format
-  if (scheme == "nekoray") {
+  else if (scheme == "nekoray") {
     needFix = false;
-    auto link = QUrl(str);
-    if (!link.isValid()){
+    if (!scheme_link.isValid()){
       goto ret_loop;
     }
-    ent = Configs::ProfileManager::NewProxyEntity(link.host());
+    ent = Configs::ProfileManager::NewProxyEntity(scheme_link.host());
     if (!ent->isValid()){
       goto ret_loop;
     }
-    if (!ent->unlock(ent->bean())->TryParseNekorayLink(link)){
+    if (!ent->unlock(ent->bean())->TryParseNekorayLink(scheme_link)){
       goto ret_loop;
     };
   }
@@ -455,12 +449,13 @@ void RawUpdater::updateSingBox(const QJsonObject &json) {
   }
 }
 
-void RawUpdater::updateWireguardFileConfig(const QString &str) {
+bool RawUpdater::updateWireguardFileConfig(const QString &str) {
   auto ent = Configs::ProfileManager::NewProxyEntity("wireguard");
   auto ok = ent->unlock(ent->WireguardBean())->TryParseLink(str);
   if (!ok)
-    return;
+    return false;
   updated_order += ent;
+  return true;
 }
 
 QString Node2QString(const fkyaml::node &n, const QString &def = "") {
@@ -527,7 +522,7 @@ fkyaml::node NodeChild(const fkyaml::node &n,
 }
 
 // https://github.com/Dreamacro/clash/wiki/configuration
-void RawUpdater::updateClash(const QString &str) {
+bool RawUpdater::updateClash(const QString &str) {
   try {
     auto proxies = fkyaml::node::deserialize(str.toStdString())["proxies"];
     for (auto proxy : proxies) {
@@ -936,7 +931,9 @@ void RawUpdater::updateClash(const QString &str) {
     //         runOnUiThread([=,this] {
     qDebug() << ("YAML Exception") << ex.what();
     //       });
+    return false;
   }
+  return true;
 }
 
 //

src/nekobox/configs/sub/GroupUpdater.hpp
@@ -15,13 +15,13 @@ namespace Subscription {
     public:
         void updateSIP008(const QJsonObject& str);
         
-        void updateClash(const QString &str);
+        bool updateClash(const QString &str);
 
         void update(const QString &str);
 
         void updateSingBox(const QJsonObject &str);
 
-        void updateWireguardFileConfig(const QString &str);
+        bool updateWireguardFileConfig(const QString &str);
 
         int gid_add_to = -1;
 

commit ba9d54ad4c4532118e8a88f586d3ad971893d30d

Fixes regression introduced in commit 60e1adcc that broke subscription parsing: 1. Fixed empty server entries - Restored proper link parsing logic using indexOf("://") instead of QUrl::isValid() which incorrectly validated empty/invalid strings 2. Restored comment filtering - Re-added checks for // and # prefixed lines 3. Restored minimum length check - Re-added check for strings shorter than 2 characters 4. Fixed missing include - Added #include <set> to ProfileFilter.cpp 5. Added PROFILE_FILTER_ENABLED option - New CMake option to enable profile filtering on subscription update (prevents clearing all servers on each update)

CMakeLists.txt
@@ -95,6 +95,7 @@ option(USE_HOTKEYS "Skip HotKeys" ON)
 option(BUILD_GO_PARTS "Build go parts" ON)
 option(CPR_USE_SYSTEM_CURL "Use system cpr" ON)
 option(CPR_ENABLE_INSTALL "Use cpr as bundled dependency" OFF)
+option(ENABLE_PROFILE_FILTER "Enable profile filtering on subscription update" ON)
 
 find_package(Threads)
 

src/gharqad/configs/sub/GroupUpdater.cpp
@@ -165,6 +165,9 @@ ret_loop:
     goto ret_loop;
   }
   */
+ if (str.startsWith("//") || str.startsWith("#") || str.length() < 2) {
+    goto ret_loop;
+  }
 
   // Multi line
   if (str.count("\n") > 0) {
@@ -195,29 +198,25 @@ parse_json:
   }
 
   QString scheme;
-  bool quic_enabled = true;
-
-  QUrl scheme_link = QUrl(str.trimmed());
-  if (scheme_link.isValid()){
-    scheme = scheme_link.scheme();
-    quic_enabled = false;
-  }
-
-  // skip if link is invalid
-  if (quic_enabled){
-
+  bool quic_enabled = false;
+  int scheme_index = str.indexOf("://");
+  if (scheme_index > 0) {
+    scheme = str.sliced(0, scheme_index).toLower();
+  } else {
+    goto ret_loop;
   }
   // Nekoray format
-  else if (scheme == "nekoray") {
+  if (scheme == "nekoray") {
     needFix = false;
-    if (!scheme_link.isValid()){
+    auto link = QUrl(str);
+    if (!link.isValid()){
       goto ret_loop;
     }
-    ent = Configs::ProfileManager::NewProxyEntity(scheme_link.host());
+    ent = Configs::ProfileManager::NewProxyEntity(link.host());
     if (!ent->isValid()){
       goto ret_loop;
     }
-    if (!ent->unlock(ent->bean())->TryParseNekorayLink(scheme_link)){
+    if (!ent->unlock(ent->bean())->TryParseNekorayLink(link)){
       goto ret_loop;
     };
   }

src/gharqad/dataStore/ProfileFilter.cpp
@@ -2,6 +2,7 @@
 #include <winsock2.h>
 #endif
 #include <nekobox/dataStore/ProfileFilter.hpp>
+#include <set>
 
 namespace Configs {
 