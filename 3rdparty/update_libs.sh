0b459b16297491f539bc516ef08237dad9bd485c
qr243vbi <qr243vbi@atomicmail.io>
2026-05-10 17:39:04 +0800

    Remove cpr

--- 3rdparty/update_libs.sh
+++ 3rdparty/update_libs.sh
@@ -9,6 +9,7 @@ rm -rf QHotkey ||:
 git clone --depth 1 https://github.com/qr243vbi/QHotkey
 rm -rf QHotkey/.git
 
+
 rm -rf quirc ||:
 git clone --depth 1 https://github.com/dlbeer/quirc.git
 rm -rf quirc/.git
@@ -17,9 +18,9 @@ mkdir qrcodegen ||:
 curl -L -o qrcodegen/qrcodegen.hpp https://raw.githubusercontent.com/nayuki/QR-Code-generator/refs/heads/master/cpp/qrcodegen.hpp
 curl -L -o qrcodegen/qrcodegen.cpp https://raw.githubusercontent.com/nayuki/QR-Code-generator/refs/heads/master/cpp/qrcodegen.cpp
 
-rm -rf cpr ||:
-git clone --depth 1 https://github.com/libcpr/cpr.git
-rm -rf cpr/.git
+#rm -rf cpr ||:
+#git clone --depth 1 https://github.com/libcpr/cpr.git
+#rm -rf cpr/.git
 
 mkdir URLParser ||:
 curl -L -o URLParser/url_parser.h https://raw.githubusercontent.com/dongbum/URLParser/refs/heads/master/src/url_parser.h
