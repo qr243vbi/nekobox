eda0d6b1578709d8baa4d1de56715e064e2d16f2
warmpain9 <warmpainq@gmail.com>
2026-06-24 15:56:36 +0300

    Revert perf default flips (FakeIP off, gvisor stack)
    Restore fake_dns=false and vpn_implementation="gvisor" defaults.
    These were just toggles of existing features; real optimization work
    will target actual code/build improvements instead.

--- src/nekobox/dataStore/DataStore.hpp
+++ src/nekobox/dataStore/DataStore.hpp
@@ -239,9 +239,9 @@ namespace Configs {
         bool adblock_enable = false;
 
         // VPN
-        bool fake_dns = true;
+        bool fake_dns = false;
         bool enable_tun_routing = false;
-        QString vpn_implementation = "system";
+        QString vpn_implementation = "gvisor";
         int vpn_mtu = 1500;
         bool vpn_ipv6 = false;
         bool vpn_strict_route = true;
